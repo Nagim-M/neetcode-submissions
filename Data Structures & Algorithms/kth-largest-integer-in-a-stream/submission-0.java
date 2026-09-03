31638167603fddeceaa2ad8a3dfd295f5f991006
N. Mohammadi <n.mohammadi@student.tudelft.nl>
2026-09-03 19:48:32 +0200

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,18 @@
+class KthLargest {
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.minHeap = new PriorityQueue<>(k);
+        this.k = k;
+        for (int num : nums) {
+            add(num);
+        }
+    }
+    
+    public int add(int val) {
+        minHeap.add(val);
+        if (minHeap.size() > k) minHeap.poll();
+        return minHeap.peek();
+    }
+}
